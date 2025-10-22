ce76a7e308423d465bd3cde4b4feb8ab2973c5c7
TienDattttt <nguyentiendat201204@gmail.com>
2025-10-22 20:24:11 +0700

    setup

---
+++ e-learning-backend/src/test/java/com/microshop/elearningbackend/ELearningBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microshop.elearningbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
