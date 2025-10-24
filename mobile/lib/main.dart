3633193de618c8e9d2ccad20526b7d999f13eac7
unknown <thanhhien271204@gmail.com>
2025-10-24 22:16:16 +0700

    Nâng tầm cuộc chơi 2.0

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Online Course App',
+        title: 'E-Learning App',
         theme: ThemeData(
           primaryColor: AppColor.primary,
         ),
